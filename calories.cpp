76c350fd587977b4df7a70757019e66d2f450e64
No one <KaliKubuntu@users.noreply.github.com>
2015-01-18 21:05:39 +0330

    Update calories.cpp

--- calories.cpp
+++ calories.cpp
@@ -1,11 +1,16 @@
-    # include <iostream>
+# include <iostream>
 
-    int main()
+int main()
     {
     using namespace std;
-    unsigned short int weight;
+    float weight;
     float length;
-    int BMI;
+    float BMI;
+    float ideal_weight_down;
+    float ideal_weight_up;
+for(;;){
+    cout<<"To get your BMI according to W. H. O's defined criteria,\n";
+    cout<<"you'll be asked for your weight and height.\n\n";
 
     cout<<"Enter your weight in kilograms: \n";
     cin>>weight;
@@ -14,31 +19,50 @@
     cin>>length;
 
     BMI=weight/ (length * length);
+    ideal_weight_down=18.5 * (length * length);
+    ideal_weight_up=24.99 * (length * length);
 
-    if (18.5<=BMI&&BMI<=20)
+    if (BMI<16.00)
     {
-        cout<<"LACK OF WEIGHT!!!\n";
+        cout<<"SEVERE THINNESS!!!\n";
     }
     else
-    if (25<BMI&&BMI<=30)
-    {
-        cout<<"Normal Weight!\n";
-    }
-    else
-    if (30<BMI&&BMI<=35)
-    {
-        cout<<"Overweight!\n";
-    }
-    else
-    if (35<BMI&&BMI<=40)
-    {
-        cout<<"OBESE!!!\n";
-    }
-    else
-    if (40<BMI||BMI<18.5)
-    {
-        cout<<"WRONG VALUE! Enter Your Correct Weight and Height!\n";
-    }
+        if (16.00<=BMI&&BMI<=16.99)
+        {
+            cout<<"MODERATE THINNESS!!\n";
+        }
+        else
+            if (17.00<=BMI&&BMI<=18.49)
+            {
+                cout<<"MILD THINNESS!\n";
+            }
+            else
+                if (18.50<=BMI&&BMI<=24.99)
+                {
+                    cout<<"Normal Range\n";
+                }
+                else
+                    if (25<=BMI&&BMI<=29.99)
+                    {
+                        cout<<"Pre-obese!\n";
+                    }
+                    else
+                        if (30<=BMI&&BMI<=34.99)
+                        {
+                            cout<<"Obese Class I!\n";
+                        }
+                        else
+                            if (35<=BMI&&BMI<=39.99)
+                            {
+                                cout<<"Obese Class II!!\n";
+                            }
+                            else
+                                if (40<=BMI)
+                                {
+                                    cout<<"Obese Class III!!!\n";
+                                }
     cout << endl << endl;
+    cout<< "Your ideal weight is between " << ideal_weight_down <<" and "<< ideal_weight_up<<".\n\n";
+}
     return 0;
     }
